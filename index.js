48e15274e9c38c8df7af27cab8bcec7e6a033b40
Ali Sindhi <64632604+MumtazAli123@users.noreply.github.com>
2023-03-21 06:38:23 +0500

    up

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
     await mongoose.connect("mongodb://localhost:27017/mydb");
      const productSchema = new mongoose.Schema({
          title: String,
-         price: Number
+         price: Number,
+         desc: String
      });
      const ProductModel = mongoose.model('mydb', productSchema);
-     let data = new ProductModel({title: 'red3', price: 3992});
+     let data = new ProductModel({title: 'apple', price: 443300, desc: 'it was new'});
      let result = await data.save();
      console.log(result);
 
