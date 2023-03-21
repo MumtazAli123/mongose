57b8ad9fab00542c066c30140a8fafced96aa476
Ali Sindhi <64632604+MumtazAli123@users.noreply.github.com>
2023-03-21 08:42:56 +0500

    updated

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
- const main = async ()=>{
-    await mongoose.connect("mongodb://localhost:27017/mydb");
-     const productSchema = new mongoose.Schema({
-         title: String,
-         price: Number,
-         desc: String
-     });
-     const ProductModel = mongoose.model('mydb', productSchema);
-     let data = new ProductModel({title: 'apple', price: 443300, desc: 'it was new'});
-     let result = await data.save();
-     console.log(result);
+mongoose.connect('mongodb://localhost:27017/mydb');
+const productSchema = new mongoose.Schema({
+  name: String,
+  title: String,
+  price: Number,
+  brand: String,
+  category: String
 
- }
- main().then(r => {
-     console.log('data insert')
- });
+});
+
+const saveInDB = async () => {
+  const Product = mongoose.model('mydbs', productSchema);
+  let data = new Product({
+    name: 'nolia  611',
+    title: "max 100",
+    price: 400,
+    brand: 'nokia',
+    category: 'Mobile'
+  });
+  const result = await data.save();
+  console.log(result);
+}
+// saveInDB();
+
+const updateInDB =async  () => {
+  const Product = mongoose.model('mydbs', productSchema);
+  let data =await  Product.updateOne(
+      { name: "iPhone x max" },
+      {
+        $set: { price: 3450, name: 'nokia' }
+      }
+  )
+  console.log(data)
+}
+updateInDB();
+
+// const deleteInDB = async ()=>{
+//   const Product = mongoose.model('mydbs', productSchema);
+//   let data = await Product.deleteOne({name:'max 100'})
+//   console.log(data);
+// }
+// const findInDB = async ()=>{
+//   const Product = mongoose.model('mydbs', productSchema);
+//   let data = await Product.find({name:'max pro 611'})
+//   console.log(data);
+// }
+
